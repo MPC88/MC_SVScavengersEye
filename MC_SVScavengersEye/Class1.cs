e92be8b12ff48c1e6439b0898198714b5b8aee9e
MPC88 <myemail@gmail.com>
2024-04-28 10:09:38 +0100

    Add project files.

---
+++ MC_SVScavengersEye/Class1.cs
@@ -0,0 +1,33 @@
+﻿using HarmonyLib;
+using BepInEx;
+using UnityEngine;
+
+namespace MC_SVScavengersEye
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class Main : BaseUnityPlugin
+    {
+        public const string pluginGuid = "mc.starvalor.scavengerseye";
+        public const string pluginName = "SV Scavenger's Eye";
+        public const string pluginVersion = "1.0.0";
+
+        public void Awake()
+        {
+            Harmony.CreateAndPatchAll(typeof(Main));
+        }
+
+        [HarmonyPatch(typeof(GameManager), nameof(GameManager.SpawnDebrisField))]
+        [HarmonyPostfix]
+        private static void SparkMoar(DebrisField debrisField)
+        {
+            if (debrisField.type == 0)
+                return;
+
+            Transform effects = debrisField.debrisFieldControl.gameObject.transform.Find("Effects");
+            if (effects == null)
+                return;
+
+            effects.localScale *= PChar.Char.explorer / 10;
+        }
+    }
+}
